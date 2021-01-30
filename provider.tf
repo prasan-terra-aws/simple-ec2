77d352340c908500061b63d48220c660d60da6a0
Prasanna R <PR@Sanju-Mac-2.local>
2021-01-30 16:47:16 +0530

    upgraded 0.14.5

--- provider.tf
+++ provider.tf
@@ -1,4 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+    source = "hashicorp/aws"
+    version = "~>3.0"
+    //region  = var.aws_region
+    }
+  }
+}
+
 provider "aws" {
   region  = var.aws_region
-  version = "~> 3.0"
 }
