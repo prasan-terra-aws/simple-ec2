9dd10a49c86b5fe1d018d3a7fc2058a215a8a7e5
prasancto <prasancto@gmail.com>
2021-02-19 07:07:03 +0000

    adding credentials variables

--- provider.tf
+++ provider.tf
@@ -9,4 +9,6 @@ terraform {
 
 provider "aws" {
   region  = var.aws_region
+  AWS_ACCESS_KEY_ID = var.AWS_ACCESS_KEY_ID
+  AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
+
+
 variable "aws_region" {
   type    = string
   default = "ap-south-1"
